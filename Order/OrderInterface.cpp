d1519e7d448b3cbebf39c8efd928d349dbc073b3
Love1KARINA <1648241930@qq.com>
2023-12-28 23:38:07 +0800

    订单部分

--- Order/OrderInterface.cpp
+++ Order/OrderInterface.cpp
@@ -5,31 +5,16 @@
 
 using namespace std;
 
-//===================== OrderInterface 订单接口类 ========================
-
-/**
- * 功能: 创建新订单
- * 参数：用户id,订单条目，价格
- */
+//新建一个订单
 void OrderInterface::CreateOrder(int customerID, map<CommodityInformation *, int> Items, float price) {
     Order *p = new Order(customerID, Items, price);
     orderlist.push_back(p);
 }
 
-/**
- * 功能: 在订单列表中增加订单
- */
-void OrderInterface::AddOrder(Order *order) {
-    orderlist.push_back(order);
-}
-
-/**
- * 功能: 在订单列表中删除订单
- * 参数：订单id
- */
+//根据id删除一个订单
 void OrderInterface::RemoveOrder(string orderno)//从订单列表中移除订单
 {
-    vector<Order *>::iterator it;
+    vector<Order*>::iterator it;
     for (it = orderlist.begin(); it != orderlist.end(); ++it) {
         if ((*it)->getID() == orderno) {
             orderlist.erase(it);
@@ -38,33 +23,14 @@ void OrderInterface::RemoveOrder(string orderno)//从订单列表中移除订单
     }
 }
 
-/**
- * 功能: 用户订单列表的展示
- * 参数：用户id
- */
-void OrderInterface::display(int id)
-{
-    CommodityInformationReader reader;
-    cout << "您的订单列表包含的订单有: " << endl;
-    for (auto order: this->orderlist) {
-        if (order->getcustomerid() == id) {
-            cout << "-起始-" << endl;
-            cout << "订单号: " << order->getID() << endl;
-            for (auto item:order->getItems()) {
-                reader.setCommodityInformation(item.first);
-                cout << "商品ID: " << reader.getID() << endl;
-                cout << "商品名称: " << reader.getName() << endl;
-                cout << "数量: " << item.second << endl;
-            }
-            order->Request();
-            cout << "-结束-" << endl;
-        }
-    }
+//在list里加一个订单
+void OrderInterface::AddOrder(Order *order) {
+    orderlist.push_back(order);
 }
 
-/**
- * 功能: 订单支付
- */
+
+
+//支付某一订单
 void OrderInterface::pay() {
     int paidOrderNum = 0;
     for (auto _order:orderlist) {
@@ -74,35 +40,51 @@ void OrderInterface::pay() {
         } else
             continue;
     }
-    printf("支付完毕!共支付了%d笔订单!\n", paidOrderNum);
+    printf("支付成功，共支付了%d笔订单\n", paidOrderNum);
 }
 
-/**
- * 功能: 用户未支付订单的展示
- * 参数：用户id
- */
+//根据用户id展示所有订单
+void OrderInterface::display(int id)
+{
+    CommodityInformationReader reader;
+    cout << "您的订单列表包含的订单有: " << endl;
+    for (auto order : this->orderlist) {
+        if (order->getcustomerid() == id) {
+            cout << "起始" << endl;
+            cout << "订单号: " << order->getID() << endl;
+            for (auto item : order->getItems()) {
+                reader.setCommodityInformation(item.first);
+                cout << "商品名: " << reader.getName() << endl;
+                cout << "商品ID: " << reader.getID() << endl;
+                cout << "数量: " << item.second << endl;
+            }
+            order->Request();
+            cout << "结束" << endl;
+        }
+    }
+}
+
+//根据用户id展示所有未支付订单
 void OrderInterface::displayUnpaid(int id) {
     CommodityInformationReader reader;
     for (auto order:orderlist) {
         if (order->getcustomerid() == id && order->getPayState() == 1) {
-            cout << "-起始-" << endl;
+            cout << "起始" << endl;
             cout << "订单号: " << order->getID() << endl;
             for (auto item:order->getItems()) {
                 reader.setCommodityInformation(item.first);
-                cout << "商品ID: " << reader.getID() << endl;
                 cout << "商品名称: " << reader.getName() << endl;
+                cout << "商品ID: " << reader.getID() << endl;
                 cout << "数量: " << item.second << endl;
             }
             order->Request();
-            cout << "-结束-" << endl;
+            cout << "结束" << endl;
         } else continue;
     }
 
 }
 
-/**
- * 功能: 选择操作
- */
+//选择操作
 void OrderInterface::MainInterface(Customer *customer) {
     int choice_status, ID;
     showUserOrderCmds showUserOrderCmds;
@@ -114,7 +96,7 @@ void OrderInterface::MainInterface(Customer *customer) {
             break;
         switch (choice_status) {
             case 1: {
-                cout << "您进行中的订单列表如下: " << endl;
+                cout << "进行中的订单列表如下: " << endl;
                 display(reader->getID());
                 break;
             }
@@ -128,7 +110,7 @@ void OrderInterface::MainInterface(Customer *customer) {
                 break;
             }
             default:
-                cout << "不存在的命令！请重新输入！" << endl;
+                cout << "输入错误，请重新输入！" << endl;
                 break;
         }
     }
--- Order/OrderInterface.h
+++ Order/OrderInterface.h
@@ -26,4 +26,4 @@ public:
 };
 
 
-#endif //DESIGNPATTERN_2021_ORDERINTERFACE_H
+#endif 
